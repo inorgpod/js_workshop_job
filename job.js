809507078d5aebb9d2206e4d9166cafa644d777d
Gao Ciaorun <itchynozz@gmail.com>
2020-04-28 17:06:10 +0800

    job_api/task_2

---
+++ job.js
@@ -0,0 +1,226 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+      Job Listings &mdash;
+      GitHub Jobs
+    </title>
+
+    <meta name="csrf-param" content="authenticity_token" />
+<meta name="csrf-token" content="lQZl4VSAoXm5Od0uzK/9LHpCm1vxUw1kOFWR10EaRLrYlcN1C5plztpyJG/hlSHWMHy9zjR9jGnOFJcAmytFjA==" />
+    <meta name="csp-nonce" content="lTCOO1f1ZrcenwpyeQ80sA==" />
+
+
+    <link rel="stylesheet" media="all" href="/assets/application-8a0cea4b3c1077a7417cbdcfe673a88324c7ba0eebeed02e897c67742f3632d7.css" />
+    <script src="/assets/application-adc6939450e5494b03c33e83d4fecdcade4843b1fe3c90ea04cd8122c9d496e8.js"></script>
+    
+  </head>
+
+  <body>
+
+    <div class="non-footer">
+      <div id="header">
+        <div id="wrap">
+          <h2 id="logo">
+            <a href="/">GitHub Jobs</a>
+          </h2>
+
+            <ul id="gnav">
+              <li><a href="/positions">All jobs</a></li>
+              <li><a href="/post">Post a job</a></li>
+              <li><a href="/faq">How it works</a></li>
+            </ul>
+        </div>
+      </div>
+
+      <div id="wrap">
+
+        
+  
+<div id="search" class="clearfix">
+
+	<form class="positions" action="/positions" accept-charset="UTF-8" method="get"><input name="utf8" type="hidden" value="&#x2713;" />
+
+		<div class="bucket description">
+			<h3>Job Description</h3>
+
+			<div class="field">
+        <input type="text" name="description" id="description_field" value="java" placeholder="Filter by title, benefits, companies, expertise" autocomplete="off" />
+			</div>
+		</div>
+
+		<div class="bucket location">
+			<h3>Location</h3>
+
+			<div class="field">
+        <input type="text" name="location" id="location_field" value="berlin" placeholder="Filter by city, state, zip code or country" autocomplete="off" />
+			</div>
+		</div>
+
+		<div class="bucket fulltime">
+			<label class="simplefield">
+        <input type="checkbox" name="full_time" id="full_time_field" value="on" checked="checked" />
+				Full Time Only
+			</label>
+		</div>
+
+		<button type="submit">Search</button>
+
+</form>
+</div>
+
+
+<div id="page">
+  <div class="inner">
+
+      <h1>
+        Showing
+          3
+        jobs
+        
+      </h1>
+
+    <div class="columns generic">
+      <div class="column main">
+        <section>
+          <table class="positionlist">
+	<tbody>
+
+			<tr class="job">
+				<td class="title">
+					<h4><a href="https://jobs.github.com/positions/ed520c47-bb94-4842-ae07-dabad1d5af42">Java Developer Back End (m/w/d) </a></h4>
+					<p class="source">
+						<a class="company" href="https://jobs.github.com/companies/administration-intelligence-ag">Administration Intelligence AG	</a>
+						–
+            <strong class="fulltime">Full Time</strong></p>
+				</td>
+
+				<td class="meta">
+          <span class="location">Berlin</span>
+          <span class="when relatize relatized">8 days ago</span>
+				</td>
+			</tr>
+			<tr class="job">
+				<td class="title">
+					<h4><a href="https://jobs.github.com/positions/f62726c8-e1d6-11e8-96fe-e8de2ac8aeb6">Backend Engineer (f/m) Routing algorithms</a></h4>
+					<p class="source">
+						<a class="company" href="https://jobs.github.com/companies/moovel-group-gmbh">moovel Group GmbH</a>
+						–
+            <strong class="fulltime">Full Time</strong></p>
+				</td>
+
+				<td class="meta">
+          <span class="location">Berlin</span>
+          <span class="when relatize relatized">over 1 year ago</span>
+				</td>
+			</tr>
+			<tr class="job">
+				<td class="title">
+					<h4><a href="https://jobs.github.com/positions/861ca26c-dc4b-11e8-8a59-311f92072400">Client-facing Software Engineer</a></h4>
+					<p class="source">
+						<a class="company" href="https://jobs.github.com/companies/datawrapper">Datawrapper</a>
+						–
+            <strong class="fulltime">Full Time</strong></p>
+				</td>
+
+				<td class="meta">
+          <span class="location">Berlin</span>
+          <span class="when relatize relatized">over 1 year ago</span>
+				</td>
+			</tr>
+
+    
+
+	</tbody>
+</table>
+
+
+        </section>
+      </div>
+
+      <div class="column sidebar">
+
+        <a class="feed" href="/positions.atom?description=java&amp;full_time=on&amp;location=berlin">Subscribe to Atom Feed</a>
+        <a class="tweet" href="https://twitter.com/GitHubJobs">Follow GitHub Jobs on Twitter</a>
+
+        <div class="module">
+  <div class="inner">
+    <h2>Subscribe to email updates</h2>
+
+    <p>Subscribe and we’ll send you a summary once a week if new jobs are posted to this list.</p>
+
+    <p>
+      <a class="button js-open-modal" href="#" rel="subscription-modal">Subscribe to updates</a>
+    </p>
+  </div>
+</div>
+
+<div id="subscription-modal" class="content popup d-none">
+  <form class="js-replace-form" id="new_subscription" data-replace-with="subscription-success" action="/subscriptions" accept-charset="UTF-8" data-remote="true" method="post"><input name="utf8" type="hidden" value="&#x2713;" />
+    <h2>Subscribe by Email</h2>
+
+    <p>We promise not to spam or share your email.</p>
+
+    <div class="field">
+      <input class="fullon" placeholder="email" type="text" name="subscription[email]" id="subscription_email" />
+    </div>
+    <input type="hidden" value="java" name="subscription[description]" id="subscription_description" />
+    <input type="hidden" value="berlin" name="subscription[location]" id="subscription_location" />
+    <input type="hidden" value="on" name="subscription[full_time]" id="subscription_full_time" />
+
+    <br/>
+
+    <button name="button" type="submit" class="button">Subscribe</button>
+</form>
+  <a href="#" class="js-close-modal close">
+    <img src="/images/facebox/closelabel.png" class="close_image" title="close">
+  </a>
+</div>
+
+<div class="d-none" id="subscription-success">
+  <h2>Subscribed!</h2>
+
+  <p>Expect an email with new listings soon!</p>
+
+  <a href="#" class="js-close-modal button">Okay</a>
+</div>
+
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+      </div>
+    </div>
+
+    <div id="footer">
+      <div class="inner">
+        <ul class="github-nav">
+          <li><a href="https://github.com/blog" class="blog">The GitHub Blog</a></li>
+          <li><a href="https://github.com/contact">Contact</a></li>
+          <li><a href="https://github.com/site/privacy">Privacy</a></li>
+          <li><a href="https://github.com/site/terms">Terms</a></li>
+            <li><a href="/api">API</a></li>
+        </ul>
+
+        <p class="copyline">
+          © 2020 GitHub Inc. All rights reserved.
+        </p>
+
+        <a href="https://github.com">
+          <img width="24" height="24" class="invertocat" src="/images/layout/invertocat.png" />
+        </a>
+
+        <!-- Environment: production -->
+      </div>
+    </div>
+
+    <div id="facebox" class="popup d-none">
+      <div id="facebox_overlay" class="js-close-modal facebox_overlayBG"></div>
+      <div class="js-modal-contents"></div>
+    </div>
+  </body>
+</html>
